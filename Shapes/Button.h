4088e70f83bbed715f448a871804be4497635c34
AnatolyKorablyov <=>
2016-04-01 07:04:52 +0300

    возможность задать бэкграунд для кнопки #6

--- Shapes/Button.h
+++ Shapes/Button.h
@@ -8,13 +8,13 @@ class CButton : public CBaseControl
 {
 public:
 	CButton();
-
+	void SetBackground(const sf::Texture & texture);
 protected:
 	void OnDraw(sf::RenderTarget& target, sf::RenderStates states) const override;
 	bool OnMousePressed(sf::Event::MouseButtonEvent const& event) override;
-
 private:
 	sf::RectangleShape m_background;
+	sf::Texture	m_backgroundTesture;
 };
 
 }
