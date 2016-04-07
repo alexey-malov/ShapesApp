b46073c483a3cf2b665649b215eab863ad993880
AnatolyKorablyov <=>
2016-04-07 23:43:25 +0300

    background

--- Shapes/BaseControl.cpp
+++ Shapes/BaseControl.cpp
@@ -3,9 +3,15 @@
 
 using namespace sf;
 using namespace std;
+using boost::find;
 
 namespace ui
 {
+
+std::shared_ptr<CBaseControl> CBaseControl::Create()
+{
+	return shared_ptr<CBaseControl>(new CBaseControl());
+}
 
 bool CBaseControl::OnEvent(sf::Event const & event)
 {
@@ -17,15 +23,60 @@ bool CBaseControl::OnEvent(sf::Event const & event)
 		|| DispatchOwnEvent(event);
 }
 
-void CBaseControl::InsertChild(const CBaseControlPtr & control, unsigned index)
+void CBaseControl::AppendChild(const CBaseControlPtr & control)
 {
-	if (index < m_children.size())
+	InsertChildAtIndex(control, GetChildCount());
+}
+
+void CBaseControl::InsertChildAtIndex(const CBaseControlPtr & control, unsigned index)
+{
+	if (!control)
 	{
-		m_children.insert(m_children.begin() + index, control);
+		throw std::invalid_argument("Invalid control");
+	}
+	if (control.get() == this)
+	{
+		throw std::invalid_argument("Can not insert itself as a child");
+	}
+	if (IsItOneOfMyParents(control))
+	{
+		throw std::invalid_argument("Can not insert any control parent as a child");
+	}
+
+	const bool itIsMyChild = (control->GetParent().get() == this);
+	if (itIsMyChild)
+	{
+		ChangeChildIndex(control, index);
 	}
 	else
 	{
-		m_children.push_back(control);
+		AdoptChild(control, index);
+	}
+}
+
+unsigned CBaseControl::GetChildCount() const
+{
+	return boost::numeric_cast<unsigned>(m_children.size());
+}
+
+CBaseControlPtr CBaseControl::GetChild(unsigned index) const
+{
+	return m_children.at(index);
+}
+
+CBaseControlPtr CBaseControl::GetParent() const
+{
+	return m_parent.lock();
+}
+
+void CBaseControl::RemoveFromParent()
+{
+	auto parent = GetParent();
+	if (parent)
+	{
+		auto self = shared_from_this();
+		parent->RemoveChild(self);
+		m_parent.reset();
 	}
 }
 
@@ -54,6 +105,75 @@ bool CBaseControl::DispatchOwnEvent(sf::Event const & event)
 	default:
 		return false;
 	}
+}
+
+void CBaseControl::SetParent(const CBaseControlPtr & parent)
+{
+	m_parent = parent;
+}
+
+void CBaseControl::RemoveChild(const CBaseControlPtr & child)
+{
+	m_children.erase(boost::remove(m_children, child), m_children.end());
+}
+
+void CBaseControl::ChangeChildIndex(const CBaseControlPtr & control, unsigned newIndex)
+{
+	assert(control);
+	assert(control->GetParent().get() == this);
+	assert(control.get() != this);
+
+	auto src = find(m_children, control);
+	assert(src != m_children.end());
+
+	if (newIndex >= GetChildCount())
+	{
+		newIndex = GetChildCount() - 1;
+	}
+
+	auto dst = m_children.begin() + newIndex;
+	if (src < dst)
+	{
+		rotate(src, src + 1, dst + 1);
+	}
+	else if (dst < src)
+	{
+		rotate(dst, src, src + 1);
+	}
+}
+
+void CBaseControl::AdoptChild(const CBaseControlPtr & control, unsigned index)
+{
+	assert(control);
+	assert(control.get() != this);
+	assert(control->GetParent().get() != this);
+
+	if (index < m_children.size())
+	{
+		m_children.insert(m_children.begin() + index, control);
+	}
+	else
+	{
+		m_children.push_back(control);
+	}
+
+	auto self = shared_from_this();
+	control->RemoveFromParent();
+	control->SetParent(self);
+}
+
+bool CBaseControl::IsItOneOfMyParents(const CBaseControlPtr & control) const
+{
+	assert(control);
+	for (auto parent = GetParent(); parent; parent = parent->GetParent())
+	{
+		if (control == parent)
+		{
+			return true;
+		}
+	}
+
+	return false;
 }
 
 bool CBaseControl::OnMousePressed(sf::Event::MouseButtonEvent const &)
--- Shapes/Button.h
+++ Shapes/Button.h
@@ -1,20 +1,27 @@
 #pragma once
 #include "BaseControl.h"
+#include "Signals.h"
 
 namespace ui
 {
 
 class CButton : public CBaseControl
 {
+	typedef Signal<void()> OnClick;
 public:
-	CButton();
-	void SetBackground(const sf::Texture & texture);
+	static std::shared_ptr<CButton> Create();
+
+	Connection DoOnClick(OnClick::slot_type const &handler);
+	void SetBackground(std::shared_ptr<sf::Texture>);
 protected:
+	CButton();
 	void OnDraw(sf::RenderTarget& target, sf::RenderStates states) const override;
 	bool OnMousePressed(sf::Event::MouseButtonEvent const& event) override;
+
 private:
-	sf::RectangleShape m_background; // для коммита
-	sf::Texture	m_backgroundTesture;
+	sf::RectangleShape m_background;
+	sf::Texture	m_backgroundTexture;
+	OnClick m_onClick;
 };
 
 }
