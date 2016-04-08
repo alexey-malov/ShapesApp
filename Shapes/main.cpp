91a5524f1aed877308727d7fbce76a64e8d07b21
Alexey Malov <alexey.malov@cpslabs.net>
2016-04-08 09:56:59 +0300

    Рефакторинг, который сломал работу тулбара

--- Shapes/main.cpp
+++ Shapes/main.cpp
@@ -10,35 +10,38 @@
 using namespace std;
 using namespace ui;
 
-int main()
+void AppendToolbar(CBaseControl & parent, const sf::Vector2u & size)
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-
-	auto root = CBaseControl::Create();
-
 	auto button = ui::CButton::Create();
 	auto button1 = ui::CButton::Create();
 
-	auto toolbar = ui::CToolBar::Create(window.getSize());
+	auto toolbar = ui::CToolBar::Create(size);
 	toolbar->AddChildWithIndex(button, 1);
 	toolbar->AddChildWithIndex(button1, 2);
 
 	toolbar->SetFrame({ 5, 50, 500, 50 });
 
-
 	std::shared_ptr<sf::Texture> background = std::make_shared<sf::Texture>();
 	background->loadFromFile("./images/wood.jpg");
 	button->SetBackground(background);
 	background->loadFromFile("./images/stainless-steel.jpg");
-	//button->SetFrame({10, 10, 300, 100});
-	//root->SetFrame({5, 50, 45, 80});
-	//root->InsertChild(button);
 
-	boost::signals2::scoped_connection con = button->DoOnClick([](){std::cout << "click" << std::endl; });
+	boost::signals2::scoped_connection con = button->DoOnClick([]() {std::cout << "click" << std::endl; });
 	boost::signals2::scoped_connection con2 = button1->DoOnClick([]() {std::cout << "clack" << std::endl; });
+
 	std::shared_ptr<sf::Texture> textureImage = std::make_shared<sf::Texture>();
 	textureImage->loadFromFile("./images/test_icon.png");
 	button->SetIcon(textureImage);
+
+	parent.AppendChild(toolbar);
+}
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+
+	auto root = CBaseControl::Create();
+	AppendToolbar(*root, window.getSize());
 
 	while (window.isOpen())
 	{
@@ -52,7 +55,6 @@ int main()
 			else
 			{
 				root->OnEvent(event);
-				toolbar->OnEvent(event);
 			}
 		}
 
@@ -60,7 +62,6 @@ int main()
 
 		{
 			window.draw(*root);
-			window.draw(*toolbar);
 		}
 
 		window.display();
